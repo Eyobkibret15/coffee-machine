afb89a3ba6a2b48378431c2c7251cdbfe9b5c16f
Mikiyas-ahmed <mikiyasahmed7@gmail.com>
2020-12-13 15:20:54 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace coffee_machine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
